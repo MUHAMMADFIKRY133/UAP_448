1f8705ff667075b380008c8e3bd737e325fcd175
MUHAMMADFIKRY133 <127368610+MUHAMMADFIKRY133@users.noreply.github.com>
2023-12-28 19:54:26 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,57 @@
+let barang = [
+    (
+        kode : "001",
+        barang : "Barang A",
+        harga : "10.000",
+    ),
+    (
+        kode : "002",
+        barang : "Barang B",
+        harga : "20.000",
+    ),
+    (
+        kode: "003",
+        barang: "Barang C",
+        harga: "30.000",
+    ),
+]
+
+function cariBarang (kode){
+    for (let i = 0; i < barang.length; i++) {
+        if (barang i = 0)  kode);
+
+
+
+    }
+    int (kode):
+
+    int (barang):
+
+    link
+}
+
+function proses (){
+
+    let kode = Document.getElementbyId("kode").value"
+
+    let jumlah = Document.getElementbyId("jumlah").value);
+
+    let barang = cariBarang =
+
+    if(!dataBarang)
+    alert ("kode barang tidak ditemukan",+ kode)
+    return
+
+}
+    alert (bayar = "total yang harus di bayar adalah", + bayar)
+
+    let (bayar = parseInt, prompt {"Bayaran"})
+
+    let (kembalian = bayar - total)
+
+    alert (kembalian ="total kembalian anda adalah", + kembalian)
+
+}
+
+
+
